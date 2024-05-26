e48b1815cdeb567374c345c688821cb4008580a2
Patrick Hu <patrickhuuu7@gmail.com>
2024-05-25 23:03:10 -0400

    Minor fix

--- website/prediction/views.py
+++ website/prediction/views.py
@@ -21,7 +21,6 @@ def process_id(request):
 def process_manual(request):
     """
     Process the manual input of 10 champions in 2 compositions.
-    The view is used both to render the index.html and process input if there is any.
     """
     if request.method == 'POST':
         top1 = request.POST.get('top1')
@@ -37,4 +36,3 @@ def process_manual(request):
         query = f"{top1},{jungle1},{mid1},{bot1},{supp1},{top2},{jungle2},{mid2},{bot2},{supp2}"
         result = get_prediction(query)
         return render(request, 'result.html', {'result': result})
-    return render(request, 'index.html')
