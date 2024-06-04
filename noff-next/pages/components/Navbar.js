cf499f972a0b45947f9de3e151ab98680c1e3371
teddio496 <teddio496@gmail.com>
2024-06-04 14:04:41 -0400

    fine tuning

--- noff-next/pages/components/Navbar.js
+++ noff-next/pages/components/Navbar.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router'
 function Navbar() {
     const router = useRouter()
   return ( 
-    <div className='navbar'> 
+    <div className='navbar' style={{position: 'fixed'}}> 
     <ul className="links">
         <li onClick={() => router.push('/')}> Home </li>
         <li onClick={() => router.push('/manual')}> Model Input </li>
--- noff-next/pages/index.js
+++ noff-next/pages/index.js
@@ -4,6 +4,47 @@ import ManualApp from './manual'
 import LiveApp from './live'
 import { motion } from 'framer-motion'
 export default function Home() {
+
+  
+
+  const onScrollStop = callback => {
+    let isScrolling;
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll',
+        e => {
+          clearTimeout(isScrolling);
+          isScrolling = setTimeout(() => {callback();}, 300);
+        },
+        false
+      );
+    }
+  };
+  
+  onScrollStop(() => {
+    console.log('The user has stopped scrolling');
+    if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+      const scrollHeight = document.documentElement.scrollHeight; // Total height of the document
+      const clientHeight = document.documentElement.clientHeight; // Height of the visible part of the document
+      const scrollTop = document.documentElement.scrollTop; // Number of pixels the document is currently scrolled from the top
+      console.log(scrollTop, clientHeight, scrollHeight)
+      // Calculate the current scroll position as a percentage of the total scroll height
+      const currentScrollPercentage = (scrollTop) / clientHeight;
+      console.log(currentScrollPercentage)
+      // Check if the current scroll position is below or above 50%
+      if (currentScrollPercentage < 0.5) {
+          // Scroll to the element with ID "liveApp" smoothly
+          document.getElementById("liveApp").scrollIntoView({ behavior: "smooth" });
+      } else {
+          // Scroll to the element with ID "manualApp" smoothly
+          document.getElementById("manualApp").scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }); 
+
+// Set an interval to check the scroll position every 150 milliseconds
+
+
+
   const scrollToLiveApp = () => {
     document.getElementById("liveApp").scrollIntoView({ behavior: "smooth" });
   };
@@ -13,7 +54,7 @@ export default function Home() {
   };
 
   return (
-    <div>
+    <div >
       <div id="liveApp" style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
         <LiveApp />
         <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={scrollToManualApp} style={{ marginTop: '100px' }}>
@@ -29,4 +70,5 @@ export default function Home() {
       </div>
     </div>
   );
+  
 }
