49fd725fe8b1278ad612140f4ad1befbbdaa5179
teddio496 <teddio496@gmail.com>
2024-06-08 18:56:32 -0400

    updated view functions

--- website/displayStats/views.py
+++ website/displayStats/views.py
@@ -9,15 +9,6 @@ from . import match_data as md
 from. import player_data as pd
 
 import requests
-import json
-
-
-def matchdata(request):
-    matchID = "NA1_4985907729"
-    api = ''
-    match = md.convert_match_to_player_data(matchID, api)
-    return HttpResponse(match['NA1_4985907729'][0]['username'])
-
 
 @api_view(['POST'])
 def load_new_match_data(request, api_key = 'RGAPI-f0816f16-3444-43d2-80d2-166c6e9a4a77'):
@@ -41,34 +32,28 @@ def load_new_match_data(request, api_key = 'RGAPI-f0816f16-3444-43d2-80d2-166c6e
     print("Successfully updated a user's data")
 
 
-def add_20_to_database(puuid, start, amount, api_key):
-    flag = False
-    req = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start={start}&count={amount}&api_key={api_key}"  
-    matches = requests.get(req)
-
-    if md.handle_riot_api_error(matches):
-        return Response({"message": matches.status_code}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    matches = matches.json()
-    print(matches)
+def add_20_to_database(puuid, api_key):
+    start = 0
+    amount = 100
     database_matches = Accounts.objects.get(puuid=puuid).past_matches
+    while True:
+        req = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start={start}&count={amount}&api_key={api_key}"  
+        matches = requests.get(req)
+        # handle error (assume it is handled for now)
+        matches = matches.json(); print(matches)
+        if len(matches) == 0: break
 
-    if not database_matches or database_matches == None: # checks if user has ever ini
-        database_matches = []
-    for match in matches:
-        if match in database_matches:
-            flag = True
-            break
-        database_matches.insert(0,match)
-        if not Matches.objects.filter(match_id=match).exists():
-            md.insert_matchdata_to_database(match, api_key)
-
-    # Saves the updated past_matches
+        for match in matches:
+            if match in database_matches: break
+            database_matches.insert(0,match)
+            if not Matches.objects.filter(match_id=match).exists():
+                md.insert_matchdata_to_database(match, api_key)
+        start += amount
     d = Accounts.objects.get(puuid=puuid)
     d.past_matches = database_matches
-    d.save()
+    d.save()  
+
     print(f"Successfully updated the past matches for the player {puuid}")
-    return flag
 
 
 @api_view(['POST'])
