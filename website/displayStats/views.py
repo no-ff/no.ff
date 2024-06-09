af18cb6e7614c992ee50075d23604b39991dc954
teddio496 <teddio496@gmail.com>
2024-06-09 02:08:59 -0400

    changed necessary functions

--- website/displayStats/views.py
+++ website/displayStats/views.py
@@ -8,13 +8,14 @@ from rest_framework import status
 
 from . import match_data as md
 from . import player_data as pd
+import time
 
 
-API_KEY = 'RGAPI-e3d577b6-1f0a-4842-888a-8c7ba7c24864'
+API_KEY = 'RGAPI-90c0f317-dfb9-4c13-8731-426f08801597   '
 
 
 @api_view(['POST'])
-def load_new_match_data(request, api_key='RGAPI-f0816f16-3444-43d2-80d2-166c6e9a4a77'):
+def load_new_match_data(request):
     # replace with how we get the ID
     form_data = request.data
     gameName = form_data.get('gameName').strip()
@@ -24,11 +25,11 @@ def load_new_match_data(request, api_key='RGAPI-f0816f16-3444-43d2-80d2-166c6e9a
     amount = 20
     start = 0
 
-    if not Accounts.objects.filter(puuid=puuid).exists():
-        pd.write_player_data(API_KEY, gameName, tagline)
-    update_past_matches(puuid)
+    # update_past_matches(puuid)
     print("Successfully updated a user's data")
-    return load_matches_from_database(0, 20, puuid)
+    print(gameName+"#"+tagline)
+    data = load_matches_from_database(0, gameName+"#"+tagline)
+    return data
 
 
 @api_view(['POST'])
@@ -40,7 +41,7 @@ def load_old_match_data(request):
     loaded_data = form_data.get('loaded').strip()
     riotID = gameName + "#" + tagline
 
-    return load_matches_from_database(loaded_data, loaded_data + 20, riotID, API_KEY)
+    return load_matches_from_database(loaded_data, riotID)
 
 
 @api_view(['POST'])
@@ -50,16 +51,17 @@ def load_player_data(request):
     print(gameName, tagline)
     if not Accounts.objects.filter(riotID=str(gameName + "#" + tagline)).exists():
         pd.write_player_data(API_KEY, gameName, tagline)
+    print("Successfully created a user's data")
     player_data = request.data
     gameName = player_data.get('gameName').strip()
     tagline = player_data.get('tagline').strip()
 
-    print(player_data)
     data = Accounts.objects.get(riotID=str(gameName + "#" + tagline))
-    print(data)
+    print(data.past_matches)
 
     sample_data = {'rank': [data.tier, data.rank, data.leaguePoints], 'wr': [data.wins, data.losses], 'sumId': data.summonerName,
                    'puuid': data.puuid, 'level': data.level, 'icon': data.icon}
+    print(sample_data)
     return Response(sample_data, status=status.HTTP_200_OK)
 
 
@@ -75,7 +77,6 @@ def update_past_matches(puuid):
         matches = requests.get(req)
         # handle error (assume it is handled for now)
         matches = matches.json()
-        print(matches)
         if len(matches) == 0:
             break
 
@@ -93,12 +94,10 @@ def update_past_matches(puuid):
     print(f"Successfully updated the past matches for the player {puuid}")
 
 
-def load_matches_from_database(start, end, riotID):
-    wanted_ids = Accounts.objects.get(
-        riotID=riotID).past_matches[start: end + 1]
+def load_matches_from_database(start, riotID):
+    wanted_ids = Accounts.objects.get(riotID=riotID).past_matches[start:start+20]
     to_put = []
     print(wanted_ids)
-    print(start, end)
     for match_id in wanted_ids:
         try:
             to_put.append(Matches.objects.get(match_id=match_id).player_data)
@@ -106,3 +105,4 @@ def load_matches_from_database(start, end, riotID):
             md.insert_matchdata_to_database(match_id, API_KEY)
             to_put.append(Matches.objects.get(match_id=match_id).player_data)
     return Response({"matches": to_put}, status=status.HTTP_200_OK)
+
