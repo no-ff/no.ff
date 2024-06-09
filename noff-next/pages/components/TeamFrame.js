d63a14066cba157b03b173dbe9d4672eea69cff5
oskip0906 <oscar.pang050906@gmail.com>
2024-06-08 21:51:34 -0400

    resolved

---
+++ noff-next/pages/components/TeamFrame.js
@@ -0,0 +1,27 @@
+
+const TeamFrame = () => {
+    return (
+        <div className="frame">
+          <div className="header">Champ Select Frame</div>
+          <div className="circleContainer">
+            <div className="circle"></div>
+            <div className="leftRows">
+              <div className="row"></div>
+              <div className="row"></div>
+              <div className="row"></div>
+              <div className="row"></div>
+              <div className="row"></div>
+            </div>
+            <div className="rightRows">
+              <div className="row"></div>
+              <div className="row"></div>
+              <div className="row"></div>
+              <div className="row"></div>
+              <div className="row"></div>
+            </div>
+          </div>
+        </div>
+    );
+};
+
+export default TeamFrame;
