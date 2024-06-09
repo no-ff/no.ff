4ff1ef2e62f2985169f78381865db27200fc6407
teddio496 <teddio496@gmail.com>
2024-06-08 21:20:50 -0400

    urls

--- website/displayStats/urls.py
+++ website/displayStats/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("add_new_matches/", views.load_new_match_data, name='load_new_match_data'),
     path("load_player_data/", views.load_player_data, name='load_player_data'),
+    path("load_old_matches/", views.load_old_match_data, name='load_old_match_data'),
 ]
