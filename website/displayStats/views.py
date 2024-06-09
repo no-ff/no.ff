d2adfdaf8c8d782a56726e7f60b8be47f5cd095e
teddio496 <teddio496@gmail.com>
2024-06-08 21:19:09 -0400

    added

--- website/displayStats/views.py
+++ website/displayStats/views.py
@@ -25,8 +25,38 @@ def load_new_match_data(request, api_key = 'RGAPI-f0816f16-3444-43d2-80d2-166c6e
         pd.write_player_data(api_key, gameName, tagline)
     update_past_matches(puuid, start, amount, api_key)
     print("Successfully updated a user's data")
+    return load_matches_from_database(0, 20, puuid, api_key)
 
 
+@api_view(['POST'])
+def load_old_match_data(request, api_key):
+    #need to count how many matches are loaded before loading more, then find the next 20 to load from the db
+    form_data = request.data
+    gameName = form_data.get('gameName').strip()
+    tagline = form_data.get('tagline').strip()
+    loaded_data = form_data.get('loaded').strip()
+    riotID = gameName + "#" + tagline
+    
+    return load_matches_from_database(loaded_data, loaded_data + 20, riotID, api_key)
+
+
+@api_view(['POST'])
+def load_player_data(request):
+    if not Accounts.objects.filter(riotID=str(gameName + "#" + tagline)).exists():
+        pd.write_player_data(gameName, tagline)
+    player_data = request.data
+    gameName = player_data.get('gameName').strip()
+    tagline = player_data.get('tagline').strip()
+
+    print(player_data)
+    data = Accounts.objects.get(riotID=str(gameName + "#" + tagline))
+    print(data)
+
+    sample_data = {'rank': [data.tier, data.rank, data.leaguePoints], 'wr': [data.wins, data.losses], 'sumId': data.summonerName,
+                    'puuid': data.puuid, 'level': data.level, 'icon': data.icon}
+    return Response(sample_data, status=status.HTTP_200_OK)
+
+"""HELPER FUNCTIONS"""
 def update_past_matches(puuid, api_key):
     start = 0
     amount = 100
@@ -51,21 +81,9 @@ def update_past_matches(puuid, api_key):
     print(f"Successfully updated the past matches for the player {puuid}")
 
 
-@api_view(['POST'])
-def load_old_match_data(request, api_key):
-    #need to count how many matches are loaded before loading more, then find the next 20 to load from the db
-    loaded_data = request.counter -1
-    player_matches = Accounts.objects.get(request.riotID).past_matches
-    if (not player_matches):
-        return 
-    add_matches = player_matches[loaded_data:loaded_data+20]
-    for match in add_matches:
-        if not Matches.objects.filter(match_id=match).exists():
-            md.insert_matchdata_to_database(match, api_key)
 
-
-def load_matches_from_database(start, end, puuid, api_key):
-    wanted_ids = Accounts.objects.get(puuid=puuid).past_matches[start: end + 1]
+def load_matches_from_database(start, end, riotID, api_key):
+    wanted_ids = Accounts.objects.get(riotID=riotID).past_matches[start: end + 1]
     to_put = []
     print(wanted_ids)
     print(start, end)
@@ -76,20 +94,3 @@ def load_matches_from_database(start, end, puuid, api_key):
             md.insert_matchdata_to_database(match_id, api_key)
             to_put.append(Matches.objects.get(match_id=match_id).player_data)
     return Response({"matches": to_put}, status=status.HTTP_200_OK)
-
-
-@api_view(['POST'])
-def load_player_data(request):
-    if not Accounts.objects.filter(riotID=str(gameName + "#" + tagline)).exists():
-        pd.write_player_data(gameName, tagline)
-    player_data = request.data
-    gameName = player_data.get('gameName').strip()
-    tagline = player_data.get('tagline').strip()
-
-    print(player_data)
-    data = Accounts.objects.get(riotID=str(gameName + "#" + tagline))
-    print(data)
-
-    sample_data = {'rank': [data.tier, data.rank, data.leaguePoints], 'wr': [data.wins, data.losses], 'sumId': data.summonerName,
-                    'puuid': data.puuid, 'level': data.level, 'icon': data.icon}
-    return Response(sample_data, status=status.HTTP_200_OK)
