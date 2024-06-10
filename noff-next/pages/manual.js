84290aa6a9cf7cdc3a7c5f3f1b4b700b87d44f71
oskip0906 <oscar.pang050906@gmail.com>
2024-06-09 22:58:03 -0400

    Changes to manual input section output

--- noff-next/pages/manual.js
+++ noff-next/pages/manual.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ManualSearchInput from './components/input/ManualSearchInput';
 
@@ -18,6 +18,14 @@ function ManualApp({ setIsManualAppShifted }) {
   });
   const [processedData, setProcessedData] = useState([]);
   const [error, setError] = useState('');
+
+  const [winrate, setWinrate] = useState(processedData[10]);
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    setWinrate(processedData[10]);
+    setKey(prevKey => prevKey + 1);
+  }, [processedData[10]]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -45,18 +53,18 @@ function ManualApp({ setIsManualAppShifted }) {
 
   return (
     <div className="App">
-      <h1 className="text-2xl font-bold mb-3">Submit Team Data</h1>
+      <h1 className="text-xl font-bold mb-3">Submit Team Data</h1>
       <form autoComplete="off" onSubmit={handleSubmit}>
         <div className="flex space-x-20">
           <div className="w-1/2">
-            <h2 className="text-xl font-semibold mb-2">Team 1</h2>
+            <h2 className="text-lg font-semibold mb-2">Team 1</h2>
             <ManualSearchInput
               label="Team 1 Top:"
               name="top1"
               value={formData.top1}
               onChange={handleChange}
               setIsManualAppShifted={setIsManualAppShifted}
-              className="mb-2 fixed"
+              className="mb-2 fixed py-2"
             />
             <ManualSearchInput
               label="Team 1 Jungle:"
@@ -64,7 +72,7 @@ function ManualApp({ setIsManualAppShifted }) {
               value={formData.jungle1}
               onChange={handleChange}
               setIsManualAppShifted={setIsManualAppShifted}
-              className="mb-2 fixed"
+              className="mb-2 fixed py-2"
             />
             <ManualSearchInput
               label="Team 1 Mid:"
@@ -72,7 +80,7 @@ function ManualApp({ setIsManualAppShifted }) {
               value={formData.mid1}
               onChange={handleChange}
               setIsManualAppShifted={setIsManualAppShifted}
-              className="mb-2 fixed"
+              className="mb-2 fixed py-2"
             />
             <ManualSearchInput
               label="Team 1 Bot:"
@@ -80,7 +88,7 @@ function ManualApp({ setIsManualAppShifted }) {
               value={formData.bot1}
               onChange={handleChange}
               setIsManualAppShifted={setIsManualAppShifted}
-              className="mb-2 fixed"
+              className="mb-2 fixed py-2"
             />
             <ManualSearchInput
               label="Team 1 Supp:"
@@ -88,11 +96,11 @@ function ManualApp({ setIsManualAppShifted }) {
               value={formData.supp1}
               onChange={handleChange}
               setIsManualAppShifted={setIsManualAppShifted}
-              className="mb-2 fixed"
+              className="mb-2 fixed py-2"
             />
           </div>
           <div className="w-1/2">
-            <h2 className="text-xl font-semibold mb-2">Team 2</h2>
+            <h2 className="text-lg font-semibold mb-2">Team 2</h2>
             <ManualSearchInput
               label="Team 2 Top:"
               name="top2"
@@ -135,45 +143,58 @@ function ManualApp({ setIsManualAppShifted }) {
             />
           </div>
         </div>
-        <button type="submit" className="mt-4 bg-blue-400 px-3 py-1 rounded">Submit</button>
+        <button type="submit" className="mt-3 bg-blue-400 px-3 py-1 rounded">Submit</button>
       </form>
       
       {processedData.length > 0 && (
-        <div>
-          <h2>Processed Data</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Team</th>
-                <th>Top</th>
-                <th>Jungle</th>
-                <th>Mid</th>
-                <th>Bottom</th>
-                <th>Support</th>
-                <th>Winning Percentage</th>
+        
+        <div className="py-8">
+          <div className="px-10 py-2 bg-gray-900" style={{ borderRadius: "var(--border-radius)", boxShadow: "0 0 10px rgba(var(--foreground-rgb), 0.1)"}}>
+            <div className="mb-3">
+              <div className="flex justify-center p-3">
+                <h1 className="font-semibold">Team 1 strength against Team 2: </h1>
+              </div>
+                <div className="winrate-bar">
+                  <div key={key} className="winrate-bar-fill flex justify-center items-center" style={{ width: '${processedData[10]}%', '--end-width': `${processedData[10]}%` }}>
+                    <span className="text-s text-blue-100 font-mono">{processedData[10]}</span>
+                  </div>
+                </div>
+            </div>
+            <table>
+              <thead>
+              <tr className="flex space-x-4">
+                <th className="py-2">Team</th>
+                <th className="py-2">Top</th>
+                <th className="py-2">Jungle</th>
+                <th className="py-2">Mid</th>
+                <th className="py-2">Bottom</th>
+                <th className="py-2">Support</th>
+                <th className="py-2">Win %</th>
               </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Team 1</td>
-                <td>{processedData[0]}</td>
-                <td>{processedData[1]}</td>
-                <td>{processedData[2]}</td>
-                <td>{processedData[3]}</td>
-                <td>{processedData[4]}</td>
-                <td>{processedData[10]}%</td>
-              </tr>
-              <tr>
-                <td>Team 2</td>
-                <td>{processedData[5]}</td>
-                <td>{processedData[6]}</td>
-                <td>{processedData[7]}</td>
-                <td>{processedData[8]}</td>
-                <td>{processedData[9]}</td>
-                <td>{processedData[11]}%</td>
-              </tr>
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                <tr className="flex space-x-4">
+                  <td className="font-bold py-1">1</td>
+                  <td className="py-1">{processedData[0]}</td>
+                  <td className="py-1">{processedData[1]}</td>
+                  <td className="py-1">{processedData[2]}</td>
+                  <td className="py-1">{processedData[3]}</td>
+                  <td className="py-1">{processedData[4]}</td>
+                  <td className="py-1 px-2 font-bold text-blue-300">{processedData[10]}%</td>
+                </tr>
+                <tr className="flex space-x-4">
+                  <td className="font-bold py-1">2</td>
+                  <td className="py-1">{processedData[5]}</td>
+                  <td className="py-1">{processedData[6]}</td>
+                  <td className="py-1">{processedData[7]}</td>
+                  <td className="py-1">{processedData[8]}</td>
+                  <td className="py-1">{processedData[9]}</td>
+                  <td className="py-1 px-2 font-bold text-blue-300">{processedData[11]}%</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <br></br>
         </div>
       )}
       
