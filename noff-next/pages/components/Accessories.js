1c47e49e340bab0722ca828b1f36c60340a73a04
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2024-06-11 18:32:53 -0400

    added-rune images

--- noff-next/pages/components/Accessories.js
+++ noff-next/pages/components/Accessories.js
@@ -1,6 +1,21 @@
 import React from 'react'
 
 function Accessories(props) {
+
+    var first = false
+    for (var path in props.playerData['runes']){
+        if (first) var secondaryPath = path;
+        else {
+            first = true;
+            var primaryPath = path;
+        }
+    }
+    const keystone = props.playerData['runes'][primaryPath][0].replace(/\s/g, '')
+    var secNum = '0';
+    var map ={'Domination': 7200, 'Inspiration': 7203, 'Precision': 7201, 'Resolve': 7204, 'Sorcery': 7202}
+
+    if (secondaryPath === 'Inspiration') secondaryPath = 'Whimsy'
+
   return (
    <div>
         <div className='champ'>
@@ -17,6 +32,11 @@ function Accessories(props) {
                     <dt><img src={'https://upload.wikimedia.org/wikipedia/commons/6/66/SmileyFace.png'}></img></dt>
                 </div>
             ))}
+        </dl>
+        <dl className='runes'>
+            <dt> <img src={'https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/' + primaryPath + '/'+ keystone + '/' +keystone + '.png'}></img></dt>
+            <dt> <img src={'https://ddragon.leagueoflegends.com/cdn/img/perk-images/Styles/' + map[secondaryPath] + '_'+ [secondaryPath] +'.png'}></img></dt>
+
         </dl>
 
 
