6534eb8e14a099fa5c2d12f7849b0c782422054b
teddio496 <teddio496@gmail.com>
2024-06-18 12:43:10 -0400

    django fixes

--- website/displayStats/views.py
+++ website/displayStats/views.py
@@ -4,28 +4,23 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from displayStats.models import Accounts, Matches
 from rest_framework import status
-from displayStats.player_data import get_match_list
-
 
 from . import match_data as md
 from . import player_data as pd
-import time
 
-API_KEY = 'RGAPI-eedfcc15-9a25-4b68-8f71-7cc6b7aa32af' 
+API_KEY = 'RGAPI-eedfcc15-9a25-4b68-8f71-7cc6b7aa32af'
+
 @api_view(['POST'])
 def load_new_match_data(request):
     # replace with how we get the ID
     form_data = request.data
-    gameName = form_data.get('gameName').strip()
-    tagline = form_data.get('tagline').strip()
-
+    gameName = form_data.get('gameName')
+    tagline = form_data.get('tagline')
     puuid = md.get_puuid(gameName, tagline, API_KEY)
-    amount = 20
-    start = 0
-
-    # update_past_matches(puuid)
+    print("Username:", gameName+"#"+tagline)
+    print("Puuid:", puuid)
+    update_past_matches(puuid)
     print("Successfully updated a user's data")
-    print(gameName+"#"+tagline)
     data = load_matches_from_database(0, gameName+"#"+tagline)
     return data
 
@@ -48,11 +43,11 @@ def load_player_data(request):
     tagline = request.data.get('tagline')
     print(gameName, tagline)
     if not Accounts.objects.filter(riotID=str(gameName + "#" + tagline)).exists():
+        print("This is a new user!!!!!")
         pd.write_player_data(API_KEY, gameName, tagline)
-    print("Successfully created a user's data")
-    player_data = request.data
-    gameName = player_data.get('gameName').strip()
-    tagline = player_data.get('tagline').strip()
+        print("Successfully created a user's data")
+    else:
+        print("This user already exists in database")
     
     data = Accounts.objects.get(riotID=str(gameName + "#" + tagline))
 
@@ -78,8 +73,8 @@ def update_past_matches(puuid):
     amount = 100
     database_row = Accounts.objects.get(puuid=puuid)
     database_matches = database_row.past_matches
-    newer_matches = get_match_list(puuid, API_KEY)
-    new = sorted(list(set(database_matches + newer_matches)))
+    newer_matches = pd.get_match_list(puuid, API_KEY)
+    new = sorted(list(set(database_matches + newer_matches)), reverse=True)
     database_row.past_matches = new
     database_row.save()
 
@@ -97,4 +92,5 @@ def load_matches_from_database(start, riotID):
         except:
             md.insert_matchdata_to_database(match_id, API_KEY)
             to_put.append(Matches.objects.get(match_id=match_id).player_data)
+    print(to_put)
     return Response({"matches": to_put}, status=status.HTTP_200_OK)
