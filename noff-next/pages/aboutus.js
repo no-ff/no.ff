f0d2d25b6899efe6499f13b5485540ea2b651d0c
Eddison Pham <eddison.pham@mail.utoronto.ca>
2024-07-28 23:57:31 -0400

    Updated About Us Page

--- noff-next/pages/aboutus.js
+++ noff-next/pages/aboutus.js
@@ -5,92 +5,93 @@ const AboutUs = () => {
         <div>
             <div class='bg about_us_bg'>
                 <div class="card">
-                    <h2 class="about_h2"><u>About NoFF</u></h2>
+                    <h2 class="about_h2"><b>ABOUT NOFF</b></h2>
                     <h1 class="about_h1">Our model predicts the win% and outcome of all the possible champion compositions. We also track past match result of any players.</h1>
                     <br></br>
-                    <h2 class="about_h2"><u>About Our Model</u></h2>
+                    <h2 class="about_h2"><b>ABOUT OUR MODEL</b></h2>
                     <h1 class="about_h1">Our machine learning model was trained with XGBoost on a test set of 50,000+ games. We can predict any combination of the 167 possible champions with 73.26% accuracy.</h1>
                 </div>
             </div>
-            <section class='credits'>
-                <div class="credits_layout">
-                    <img alt="person"></img>
-                    <div class="credits_text">
-                        <h2 class="about_h2">Name/Position</h2>
-                        <h1 class="about_h1">Description</h1>
-                        <p class="about_p">LinkedIn: </p>
-                        <p class="about_p">GitHub: </p>
-                        <p class="about_p">Phone Number:</p>
-                        <p class="about_p">Email:</p>
+            <div class="credits_section">
+                <div class="credits_title">
+                    <div class='credits_title_card'>
+                        <h3 class="about_h3"><i>CREDITS TO OUR DEVELOPMENT TEAM</i></h3>
                     </div>
                 </div>
-                <div class="credits_layout">
-                    <img alt="person"></img>
-                    <div class="credits_text">
-                        <h2 class="about_h2">Name/Position</h2>
-                        <h1 class="about_h1">Description</h1>
-                        <p class="about_p">LinkedIn: </p>
-                        <p class="about_p">GitHub: </p>
-                        <p class="about_p">Phone Number:</p>
-                        <p class="about_p">Email:</p>
+                <div class='credits'>
+                    <div class="credits_layout">
+                        <img class="oscar" alt="person"></img>
+                        <div class="credits_text">
+                            <h2 class="about_h2"><b>Oscar Peng/Machine Learning</b></h2>
+                            <p class="about_p"><u>LinkedIn:</u></p>
+                            <p class="about_p"><u>GitHub:</u> </p>
+                            <p class="about_p"><u>Phone Number:</u></p>
+                            <p class="about_p"><u>Email:</u></p>
+                        </div>
+                    </div>
+                    <div class="credits_layout">
+                        <img class="eddy"  alt="person"></img>
+                        <div class="credits_text">
+                            <h2 class="about_h2"><b>Eddison Pham/Machine Learning</b></h2>
+                            <p class="about_p"><u>LinkedIn:</u> https://www.linkedin.com/in/eddison-pham-5b8a79297/ </p>
+                            <p class="about_p"><u>GitHub</u>: https://github.com/eddisonpham</p>
+                            <p class="about_p"><u>Phone Number</u>: 416-520-5379</p>
+                            <p class="about_p"><u>Email:</u> eddison.pham@mail.utoronto.ca</p>
+                        </div>
+                    </div>
+                    <div class="credits_layout">
+                        <img class="teddy" alt="person"></img>
+                        <div class="credits_text">
+                            <h2 class="about_h2"><b>Name/Position</b></h2>
+                            <p class="about_p"><u>LinkedIn:</u></p>
+                            <p class="about_p"><u>GitHub</u>:</p>
+                            <p class="about_p"><u>Phone Number</u>:</p>
+                            <p class="about_p"><u>Email:</u></p>
+                        </div>
+                    </div>
+                    <div class="credits_layout">
+                        <img class="evan" alt="person"></img>
+                        <div class="credits_text">
+                            <h2 class="about_h2"><b>Name/Position</b></h2>
+                            <p class="about_p"><u>LinkedIn:</u></p>
+                            <p class="about_p"><u>GitHub</u>:</p>
+                            <p class="about_p"><u>Phone Number</u>:</p>
+                            <p class="about_p"><u>Email:</u></p>
+                        </div>
+                    </div>
+                    <div class="credits_layout">
+                        <img class="albert" alt="person"></img>
+                        <div class="credits_text">
+                            <h2 class="about_h2"><b>Name/Position</b></h2>
+                            <p class="about_p"><u>LinkedIn:</u></p>
+                            <p class="about_p"><u>GitHub</u>:</p>
+                            <p class="about_p"><u>Phone Number</u>:</p>
+                            <p class="about_p"><u>Email:</u></p>
+                        </div>
+                    </div>
+                    <div class="credits_layout">
+                        <img alt="person"></img>
+                        <div class="credits_text">
+                            <h2 class="about_h2"><b>Name/Position</b></h2>
+                            <p class="about_p"><u>LinkedIn:</u></p>
+                            <p class="about_p"><u>GitHub</u>:</p>
+                            <p class="about_p"><u>Phone Number</u>:</p>
+                            <p class="about_p"><u>Email:</u></p>
+                        </div>
+                    </div>
+                    <div class="credits_layout">
+                        <img alt="person"></img>
+                        <div class="credits_text">
+                            <h2 class="about_h2"><b>Name/Position</b></h2>
+                            <p class="about_p"><u>LinkedIn:</u></p>
+                            <p class="about_p"><u>GitHub</u>:</p>
+                            <p class="about_p"><u>Phone Number</u>:</p>
+                            <p class="about_p"><u>Email:</u></p>
+                        </div>
                     </div>
                 </div>
-                <div class="credits_layout">
-                <img alt="person"></img>
-                    <div class="credits_text">
-                    <h2 class="about_h2">Name/Position</h2>
-                        <h1 class="about_h1">Description</h1>
-                        <p class="about_p">LinkedIn: </p>
-                        <p class="about_p">GitHub: </p>
-                        <p class="about_p">Phone Number:</p>
-                        <p class="about_p">Email:</p>
-                    </div>
-                </div>
-                <div class="credits_layout">
-                <img alt="person"></img>
-                    <div class="credits_text">
-                    <h2 class="about_h2">Name/Position</h2>
-                        <h1 class="about_h1">Description</h1>
-                        <p class="about_p">LinkedIn: </p>
-                        <p class="about_p">GitHub: </p>
-                        <p class="about_p">Phone Number:</p>
-                        <p class="about_p">Email:</p>
-                    </div>
-                </div>
-                <div class="credits_layout">
-                <img alt="person"></img>
-                    <div class="credits_text">
-                    <h2 class="about_h2">Name/Position</h2>
-                        <h1 class="about_h1">Description</h1>
-                        <p class="about_p">LinkedIn: </p>
-                        <p class="about_p">GitHub: </p>
-                        <p class="about_p">Phone Number:</p>
-                        <p class="about_p">Email:</p>
-                    </div>
-                </div>
-                <div class="credits_layout">
-                <img alt="person"></img>
-                    <div class="credits_text">
-                        <h2 class="about_h2">Name/Position</h2>
-                        <h1 class="about_h1">Description</h1>
-                        <p class="about_p">LinkedIn: </p>
-                        <p class="about_p">GitHub: </p>
-                        <p class="about_p">Phone Number:</p>
-                        <p class="about_p">Email:</p>
-                    </div>
-                </div>
-                <div class="credits_layout">
-                <img alt="person"></img>
-                    <div class="credits_text">
-                        <h2 class="about_h2">Name/Position</h2>
-                        <h1 class="about_h1">Description</h1>
-                        <p class="about_p">LinkedIn: </p>
-                        <p class="about_p">GitHub: </p>
-                        <p class="about_p">Phone Number:</p>
-                        <p class="about_p">Email:</p>
-                    </div>
-                </div>
-            </section>
+            </div>
+            
         </div>
         
     );
